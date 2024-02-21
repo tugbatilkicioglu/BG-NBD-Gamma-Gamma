38107d6014bc4da2e144149e0b3c42b837192491
Tuğba TİLKİCİOĞLU <146488320+tugbatilkicioglu@users.noreply.github.com>
2024-02-21 19:05:39 +0300

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-############   CLTV Forecast with BG/NBD and Gamma-Gamma   ############
-
-# Y shoes company wants to determine a roadmap for sales and marketing activities. In order for the company to make medium-long term
-# plans, it is necessary to estimate the potential value that existing customers will provide to the company in the
-# future. The data set consists of information obtained from the past shopping behavior of customers who made their
-# last purchases from Y shoes company via OmniChannel (both online and offline shopping) in 2020 - 2021.
-
-# - master_id:   Unique customer number
-# - order_channel: Which channel of the shopping platform is used (Android, iOS, Desktop, Mobile)
-# - last_order_channel: Channel where last purchase was made
-# - first_order_date: Date of the customer's first purchase
-# - last_order_date: Customer's last purchase date
-# - last_order_date_online: The customer's last shopping date on the online platform
-# - last_order_date_offline: The last shopping date of the customer on the offline platform
-# - order_num_total_ever_online: Total number of purchases made by the customer on the online platform
-# - order_num_total_ever_offline: Total number of purchases made by the customer offline
-# - customer_value_total_ever_offline: Total fee paid by the customer for offline purchases
-# - customer_value_total_ever_online: Total fee paid by the customer for online shopping
-# - interested_in_categories_12: List of categories the customer has shopped in the last 12 months
 
 #!pip install lifetimes
 import pandas as pd
